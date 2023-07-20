71e33b973c65de45b5adbf184f46cb55b677f812
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2023-07-21 00:03:53 +0700

    handle status settlement

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -85,7 +85,7 @@ class TransactionController extends Controller
         $serverKey = config('midtrans.server_key');
         $hashed = hash('sha512', $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
         if ($hashed == $request->signature_key) {
-            if ($request->transaction_status == 'capture') {
+            if ($request->transaction_status == 'capture' || 'settlement') {
                 $transaction = Transaction::where('order_id', $request->order_id)->first();
                 $transaction->update([
                     'status' => 'paid',
