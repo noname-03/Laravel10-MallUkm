09c9e8f5d44ce64c9fc1d3f2d400cc36599e1248
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2023-07-20 10:11:05 +0700

    change response image url in category

--- app/Http/Controllers/Api/CategoryProductController.php
+++ app/Http/Controllers/Api/CategoryProductController.php
@@ -11,6 +11,10 @@ class CategoryProductController extends Controller
     public function index()
     {
         $category = CategoryProduct::all();
+        $category->map(function ($category) {
+            $category->photo = asset('images/category/' . $category->photo);
+            return $category;
+        });
         return response()->json([
             'code' => 200,
             'message' => 'Berhasil Mengambil Data Kategory',
