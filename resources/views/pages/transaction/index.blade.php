1536b31d409ad7f403737f35e2c04e2030afcee2
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2023-08-03 21:32:51 +0700

    add condition status button in index transaction

--- resources/views/pages/transaction/index.blade.php
+++ resources/views/pages/transaction/index.blade.php
@@ -1,142 +1,148 @@
 @extends('layouts.app')
 @section('title', 'Transaksi')
 @push('styles')
-<!-- third party css -->
-<link href="{{ asset('/') }}assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet"
-    type="text/css" />
-<link href="{{ asset('/') }}assets/libs/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css"
-    rel="stylesheet" type="text/css" />
-<link href="{{ asset('/') }}assets/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css" rel="stylesheet"
-    type="text/css" />
-<link href="{{ asset('/') }}assets/libs/datatables.net-select-bs5/css//select.bootstrap5.min.css" rel="stylesheet"
-    type="text/css" />
-<!-- third party css end -->
+    <!-- third party css -->
+    <link href="{{ asset('/') }}assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('/') }}assets/libs/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css"
+        rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/') }}assets/libs/datatables.net-buttons-bs5/css/buttons.bootstrap5.min.css" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('/') }}assets/libs/datatables.net-select-bs5/css//select.bootstrap5.min.css" rel="stylesheet"
+        type="text/css" />
+    <!-- third party css end -->
 @endpush
 
 @section('content')
-<div class="container-fluid">
+    <div class="container-fluid">
 
-    <!-- start page title -->
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
-                        <li class="breadcrumb-item active">Transaksi</li>
-                    </ol>
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box">
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Transaksi</li>
+                        </ol>
+                    </div>
+                    <h4 class="page-title">Data Transaksi</h4>
                 </div>
-                <h4 class="page-title">Data Transaksi</h4>
             </div>
         </div>
-    </div>
-    <!-- end page title -->
+        <!-- end page title -->
 
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="header-title">Data Transaksi</h4>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="header-title">Data Transaksi</h4>
 
-                    <table id="datatable-buttons" class="table dt-responsive nowrap w-100">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>User</th>
-                                <th>Order ID</th>
-                                <th>Total</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
+                        <table id="datatable-buttons" class="table dt-responsive nowrap w-100">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>User</th>
+                                    <th>Order ID</th>
+                                    <th>Total</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
 
-                        <tbody>
-                            @foreach ($transactions as $item)
-                            <tr>
-                                <td>{{$loop->iteration}}</td>
-                                <td>{{$item->user->name}}</td>
-                                <td>{{$item->order_id}}</td>
-                                <td>@currency($item->total)</td>
-                                <td>
-                                    @if ($item->status === 'paid')
-                                    <button class="btn btn-sm btn-success">Paid</button>
-                                    @elseif($item->status === 'unpaid')
-                                    <button class="btn btn-sm btn-info">Unpaid</button>
-                                    @else
-                                    <button class="btn btn-sm btn-danger">Cenceled</button>
-                                    @endif
-                                </td>
-                                <td>
-                                    <form action="{{ route('transaction.destroy', $item->id) }}" method="POST">
-                                        @method('DELETE') @csrf
-                                        <div class="btn-group" role="group" aria-label="Basic example">
-                                            <a href="{{ route('transaction.show', $item->id) }}"
-                                                class="btn btn-sm btn-outline-info">
-                                                Show
-                                            </a>
-                                            <a href="{{ route('transaction.edit', $item->id) }}"
-                                                class="btn btn-sm btn-outline-secondary">
-                                                Edit
-                                            </a>
-                                            {{-- <button type="submit"
+                            <tbody>
+                                @foreach ($transactions as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->user->name }}</td>
+                                        <td>{{ $item->order_id }}</td>
+                                        <td>@currency($item->total)</td>
+                                        <td>
+                                            @if ($item->status === 'paid')
+                                                <button class="btn btn-sm btn-success">Paid</button>
+                                            @elseif($item->status === 'unpaid')
+                                                <button class="btn btn-sm btn-info">Unpaid</button>
+                                            @elseif($item->status === 'sending')
+                                                <button class="btn btn-sm btn-warning">Sending</button>
+                                            @elseif($item->status === 'delivered')
+                                                <button class="btn btn-sm btn-primary">Delivered</button>
+                                            @else
+                                                <button class="btn btn-sm btn-danger">Cenceled</button>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <form action="{{ route('transaction.destroy', $item->id) }}" method="POST">
+                                                @method('DELETE') @csrf
+                                                <div class="btn-group" role="group" aria-label="Basic example">
+                                                    <a href="{{ route('transaction.show', $item->id) }}"
+                                                        class="btn btn-sm btn-outline-info">
+                                                        Show
+                                                    </a>
+                                                    <a href="{{ route('transaction.edit', $item->id) }}"
+                                                        class="btn btn-sm btn-outline-secondary">
+                                                        Edit
+                                                    </a>
+                                                    {{-- <button type="submit"
                                                 onclick="return confirm('Apakah Anda Yakin Ingin Menghapus Data Ini?')"
                                                 class="btn btn-sm btn-outline-danger">
                                                 Delete
                                             </button> --}}
-                                        </div>
-                                    </form>
+                                                </div>
+                                            </form>
 
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
 
-                </div> <!-- end card body-->
-            </div> <!-- end card -->
-        </div><!-- end col-->
+                    </div> <!-- end card body-->
+                </div> <!-- end card -->
+            </div><!-- end col-->
+        </div>
+        <!-- end row-->
     </div>
-    <!-- end row-->
-</div>
 @endsection
 @push('scripts')
-<!-- third party js -->
-<script src="{{ asset('/') }}assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-buttons/js/buttons.flash.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/datatables.net-select/js/dataTables.select.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/pdfmake/build/pdfmake.min.js"></script>
-<script src="{{ asset('/') }}assets/libs/pdfmake/build/vfs_fonts.js"></script>
-<!-- third party js ends -->
+    <!-- third party js -->
+    <script src="{{ asset('/') }}assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-buttons/js/buttons.flash.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/datatables.net-select/js/dataTables.select.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/pdfmake/build/pdfmake.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/pdfmake/build/vfs_fonts.js"></script>
+    <!-- third party js ends -->
 
-<!-- Datatables init -->
-<script src="{{ asset('/') }}assets/js/pages/datatables.init.js"></script>
+    <!-- Datatables init -->
+    <script src="{{ asset('/') }}assets/js/pages/datatables.init.js"></script>
 
-<script>
-    $(function() {
-        $("#example3").DataTable({
-            "responsive": true,
-            "lengthChange": false,
-            "autoWidth": false,
-            "buttons": [
-                {
-                extend: 'pdf',
-                exportOptions: {
-                    columns: 'th:not(:last-child)'
-                }},
-                {
-                extend: 'excel',
-                exportOptions: {
-                    columns: 'th:not(:last-child)'
-                }}],
-        }).buttons().container().appendTo('#example3_wrapper .col-md-6:eq(0)');
-    });
-</script>
+    <script>
+        $(function() {
+            $("#example3").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+                "buttons": [{
+                        extend: 'pdf',
+                        exportOptions: {
+                            columns: 'th:not(:last-child)'
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        exportOptions: {
+                            columns: 'th:not(:last-child)'
+                        }
+                    }
+                ],
+            }).buttons().container().appendTo('#example3_wrapper .col-md-6:eq(0)');
+        });
+    </script>
 @endpush
