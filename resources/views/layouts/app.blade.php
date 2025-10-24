239c0a3366d6874e492485b83307728a4bd24d53
Fahrurrozi <79890528+noname-03@users.noreply.github.com>
2025-10-24 19:08:59 +0700

    add meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('/') }}assets/images/logoMall.png">
